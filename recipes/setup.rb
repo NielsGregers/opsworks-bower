d70ebd57997bfcb09549401521538a025d6255bf
Leonid Beder <leonid@jammer.fm>
2015-04-04 01:43:00 +0300

    Typo.

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
-    Chef::Log.debug("Skipping application #{application} as it is not an Rails app")
+    Chef::Log.debug("Skipping application #{application} as it is not a Rails app")
     next
   end
 
