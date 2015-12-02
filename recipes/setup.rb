e1aab79adf10019b64d7a991834438d42a3c0fd4
NielsGregers <niels@timeboxer.dk>
2015-12-02 15:46:48 +0100

    Update setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,8 +1,4 @@
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'rails'
-    Chef::Log.debug("Skipping application #{application} as it is not a Rails app")
-    next
-  end
 
   execute 'install bower' do
     command 'npm install -g bower'
